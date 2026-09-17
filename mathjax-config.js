4397f95ef0015139f2d1977e2cb74d37174d2073
Fan Yang <fan.yang@edu.ruhr-uni-bochum.de>
2026-09-17 08:32:12 +0200

    Support LaTeX formulas with MathJax

---
+++ mathjax-config.js
@@ -0,0 +1,9 @@
+// Use \(...\) for inline mathematics and \[...\] for display equations.
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],
+    processEscapes: true
+  },
+  svg: { fontCache: 'global' }
+};
